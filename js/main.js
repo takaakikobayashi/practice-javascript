3156b67374cdacc465d5b71dc358b25309514362
小林孝明 <takakoba0426@gmail.com>
2020-07-08 12:44:25 +0900

    temporary save

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+
+  var price = document.getElementById('price');
+  var num = document.getElementById('num');
+  var unit = document.getElementById('unit');
+  var btn = document.getElementById('btn');
+  var reset = document.getElementById('reset');
+  var result = document.getElementById('result');
+
+  btn.addEventListener('click', function() {
+
+  });
+})();
