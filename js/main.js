0457148d14f519be02a7e5c35b13de83ef9526ca
小林孝明 <takakoba0426@gmail.com>
2020-07-11 20:00:51 +0900

    temporary save

--- js/main.js
+++ js/main.js
@@ -9,6 +9,18 @@
   var result = document.getElementById('result');
 
   btn.addEventListener('click', function() {
+    var payless;
+    var short;
+    var paymore;
+    var over;
+    var str;
 
-  });
+    payless = Math.floor(price.value / num.value / unit.value) * unit.value;
+    short = price.value - (payless * num.value);
+    paymore = Math.ceil(price.value / num.value / unit.value) * unit.value;
+    over = Math.abs(price.value - (paymore * num.value));
+    str = 
+      '一人' + payless + '円だと' + short + '円足りません' +
+      '一人' + paymore + '円だと' + over+ '円余ります'
+  }); 
 })();
